f5e3f3298f02913300231c6f05e8bb4ae84c392c
samorey11 <samorey@outlook.com>
2022-11-17 22:07:00 -0500

    Completed User & Session with failures

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_10_043639) do
+ActiveRecord::Schema.define(version: 2022_11_18_024009) do
+
+  create_table "sessions", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.string "token"
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.datetime "created_at", null: false
